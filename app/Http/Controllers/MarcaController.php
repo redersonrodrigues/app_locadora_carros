a1423a0c46ada4e80088949d1b046bd052d88181
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-06-23 10:20:28 -0300

    292. Extra (fix da aula anterior) - Atributo $namespace de RouteServiceProvide 293. Criando registros via POST

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -16,33 +16,18 @@ class MarcaController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $marca = Marca::create($request->all());
+        return $marca;
     }
 
     /**
      * Display the specified resource.
      */
     public function show(Marca $marca)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Marca $marca)
     {
         //
     }
