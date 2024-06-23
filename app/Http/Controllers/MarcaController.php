f6eae7014fce84d656324678dbf63bb00528aa4a
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-06-23 11:55:47 -0300

    297. Extra - Entendendo o conceito de endpoint (URL, URN e URI) 298. Injetando a instância do Model no Controller e como lidar com o Type Hinting

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,12 +7,18 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+
+public function __construct(Marca $marca) {
+    $this->marca = $marca;
+}
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $marcas = Marca::all();
+        //$marcas = Marca::all();
+        $marcas = $this->marca->all();
         return $marcas;
     }
 
@@ -21,37 +27,49 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
+        // $marca = Marca::create($request->all());
+        $marca = $this->marca->create($request->all());
         return $marca;
     }
 
     /**
+     * @param Integer
      * Display the specified resource.
+     * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show( $id )
     {
+        $marca = $this->marca->find($id);
         return $marca;
     }
 
     /**
      * Update the specified resource in storage.
+     * @param \Illuminate\Http\Request $request
+     * @param Integer
+     * @return \Illuminate\Http\Response
+     * 
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
         // print_r($request->all()); // dados atualizados
         // echo   '<hr>';
         // print_r($marca->getAttributes()); // dados antigos
 
+        $marca = $this->marca->find($id);
         $marca->update($request->all());
+
         return $marca;
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @param Integer
+    * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
         // print_r($marca->getAttributes());
+        $marca = $this->marca->find($id);
         $marca->delete();
         return ['msg' => 'A marca foi removida com sucesso!'];
     }
