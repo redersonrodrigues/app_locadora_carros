1e20189b7e8a55530f38158b0c80fbdd0e82f72c
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-06-23 14:02:56 -0300

    302. Implementando as regras de validação (Rules) no Model

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -30,16 +30,8 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        // $marca = Marca::create($request->all());
-        $regras = [
-            'nome'      => 'required|unique:marcas',
-            'imagem'    => 'required'
-        ];
-        $feedback = [
-            'required' => 'O campo :attribute é obrigatório',
-            'nome.unique' => 'Já existe uma marca com este nome',
-        ];
-        request()->validate($regras, $feedback);
+        // validação
+        request()->validate($this->marca->rules(), $this->marca->feedback());
 
         $marca = $this->marca->create($request->all());
         return response()->json($marca, 201);
--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -9,4 +9,21 @@ class Marca extends Model
 {
     use HasFactory;
     protected $fillable = ['nome', 'imagem'];
+
+    public function rules()
+    {
+        return [
+            'nome'      => 'required|unique:marcas|min:3',
+            'imagem'    => 'required'
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required'      => 'O campo :attribute é obrigatório',
+            'nome.unique'   => 'Já existe uma marca com este nome',
+            'nome.min'      => 'O nome deve ter no mínimo 3 caracteres',
+        ];
+    }
 }
