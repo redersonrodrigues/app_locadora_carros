58d5f82c67a743669085c9e0201003f1e9ba5c05
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-06-23 13:53:09 -0300

    300. Validações parte 2 - Status Code HTTP 301. Validações parte 3 - Validando parâmetros e a importância do Accept

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Marca;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation;
 
 class MarcaController extends Controller
 {
 
-public function __construct(Marca $marca) {
-    $this->marca = $marca;
-}
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
 
     /**
      * Display a listing of the resource.
@@ -19,7 +22,7 @@ public function __construct(Marca $marca) {
     {
         //$marcas = Marca::all();
         $marcas = $this->marca->all();
-        return $marcas;
+        return response()->json($marcas, 200);
     }
 
     /**
@@ -28,8 +31,18 @@ public function __construct(Marca $marca) {
     public function store(Request $request)
     {
         // $marca = Marca::create($request->all());
+        $regras = [
+            'nome'      => 'required|unique:marcas',
+            'imagem'    => 'required'
+        ];
+        $feedback = [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'Já existe uma marca com este nome',
+        ];
+        request()->validate($regras, $feedback);
+
         $marca = $this->marca->create($request->all());
-        return $marca;
+        return response()->json($marca, 201);
     }
 
     /**
@@ -37,13 +50,13 @@ public function __construct(Marca $marca) {
      * Display the specified resource.
      * @return \Illuminate\Http\Response
      */
-    public function show( $id )
+    public function show($id)
     {
         $marca = $this->marca->find($id);
         if ($marca === null) {
-            return ['erro' => 'Recurso pesquisado não existe'];
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
         }
-        return $marca;
+        return response()->json($marca, 200);
     }
 
     /**
@@ -58,25 +71,25 @@ public function __construct(Marca $marca) {
 
         $marca = $this->marca->find($id);
         if ($marca === null) {
-            return ['erro' => 'Impossível realizar a atualização, o recurso pesquisado não existe'];
+            return response()->json(['erro' => 'Impossível realizar a atualização, o recurso pesquisado não existe'], 404);
         }
         $marca->update($request->all());
 
-        return $marca;
+        return response()->json($marca, 200);
     }
 
     /**
      * @param Integer
-    * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         // print_r($marca->getAttributes());
         $marca = $this->marca->find($id);
         if ($marca === null) {
-            return ['erro' => 'Impossível deletar o registro, o recurso pesquisado não existe'];
+            return response()->json(['erro' => 'Impossível deletar o registro, o recurso pesquisado não existe'], 404);
         }
         $marca->delete();
-        return ['msg' => 'A marca foi removida com sucesso!'];
+        return response()->json(['msg' => 'A marca foi removida com sucesso!'], 200);
     }
 }
