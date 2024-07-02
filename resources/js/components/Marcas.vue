e1a7529233ddd6f1a0e59615f6189bd804c2b1ff
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-07-02 10:57:16 -0300

    352. Criando o card de busca de registros

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -3,10 +3,27 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Marcas</div>
+                    <div class="card-header">Busca de marcas</div>
 
                     <div class="card-body">
-                        <p>Vamos manipular os registros de marcas por aqui!</p>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <label for="inputId" class="form-label">ID</label>
+                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
+                                    placeholder="ID">
+                                <div id="idHelp" class="form-text text-muted">Opcional. Informe o ID do registro</div>
+                            </div>
+                            <div class="col">
+                                <label for="inputNome" class="form-label">Nome</label>
+                                <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp"
+                                    placeholder="Nome da marca">
+                                <div id="nomeHelp" class="form-text text-muted">Opcional. Informe o nome da marca</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary btn-sm float-end">Pesquisar</button>
                     </div>
                 </div>
             </div>
@@ -15,5 +32,5 @@
 </template>
 
 <script>
-
+    // O código JavaScript pode ir aqui, se necessário.
 </script>
