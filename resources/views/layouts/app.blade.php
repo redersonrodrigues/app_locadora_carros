ab1e9169ef137241d6226edaa3ab5f195885c14a
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-07-03 10:49:35 -0300

    357. Extra - Adicionando um breadcrumb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
                                 <div class="dropdown-menu">
                                     <a class="dropdown-item" href="#">Carros</a>
                                     <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="{{route('marcas')}}">Marcas</a>
+                                    <a class="dropdown-item" href="{{ route('marcas') }}">Marcas</a>
                                     <a class="dropdown-item" href="#">Modelos</a>
                                 </div>
                             </li>
@@ -94,6 +94,15 @@
                 </div>
             </div>
         </nav>
+        @auth
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">{{ Route::currentRouteName() }}</li>
+                </ol>
+            </nav>
+        @endauth
+
 
         <main class="py-4">
             @yield('content')
