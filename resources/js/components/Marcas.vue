c858f83462ecd6b42c312bfb6968efc6182bda4b
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-07-03 13:47:39 -0300

    359. Adicionando os inputs no modal de registros de marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -2,24 +2,28 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                
+
                 <!-- início do card de busca -->
                 <card-component titulo="Busca de marcas">
                     <template v-slot:conteudo>
                         <div class="row g-3">
                             <div class="col mb-3">
-                                <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o ID da marca">
-                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
-                                </input-container-component>                   
+                                <input-container-component titulo="ID" id="inputId" id-help="idHelp"
+                                    texto-ajuda="Opcional. Informe o ID da marca">
+                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
+                                        placeholder="ID">
+                                </input-container-component>
                             </div>
                             <div class="col mb-3">
-                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o nome da marca">
-                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp"
+                                    texto-ajuda="Opcional. Informe o nome da marca">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp"
+                                        placeholder="Nome da marca">
                                 </input-container-component>
                             </div>
                         </div>
                     </template>
-                    
+
                     <template v-slot:rodape>
                         <button type="submit" class="btn btn-primary btn-sm float-end">Pesquisar</button>
                     </template>
@@ -34,14 +38,38 @@
                     </template>
 
                     <template v-slot:rodape>
-                        <button type="button" class="btn btn-primary btn-sm float-end"  data-bs-toggle="modal" data-bs-target="#modalMarca">Adicionar</button>
+                        <button type="button" class="btn btn-primary btn-sm float-end" data-bs-toggle="modal"
+                            data-bs-target="#modalMarca">Adicionar</button>
                     </template>
                 </card-component>
                 <!-- fim do card de listagem de marcas -->
             </div>
         </div>
-        <modal-component id="modalMarca" titulo="Adicionar marca"></modal-component>
-         </div>
+        <modal-component id="modalMarca" titulo="Adicionar marca">
+            <template v-slot:conteudo>
+                <div class="form-group">
+                    <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp"
+                        texto-ajuda="Informe o nome da marca">
+                        <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp"
+                            placeholder="Nome da marca">
+                    </input-container-component>
+                </div>
+
+                <div class="form-group">
+                    <input-container-component titulo="Imagem" id="novoImagem" id-help="novoImagemHelp"
+                        texto-ajuda="Selecione uma imagem no formato PNG">
+                        <input type="file" class="form-control" id="novoImagem" aria-describedby="novoImagemHelp"
+                            placeholder="Selecione uma imagem">
+                    </input-container-component>
+                </div>
+            </template>
+
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary">Salvar</button>
+            </template>
+        </modal-component>
+    </div>
 </template>
 
 <script>
--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -7,11 +7,10 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <p>Modal body text goes here.</p>
+                    <slot name="conteudo"></slot>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                    <button type="button" class="btn btn-primary">Salvar</button>
+                    <slot name="rodape"></slot>
                 </div>
             </div>
         </div>
