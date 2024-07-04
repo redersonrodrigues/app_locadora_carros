9efe595554f72074e01760e4962fd22d81fcca04
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-07-04 20:58:41 -0300

    369. Listando os registros de marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -34,7 +34,10 @@
                 <!-- início do card de listagem de marcas -->
                 <card-component titulo="Relação de marcas">
                     <template v-slot:conteudo>
-                        <table-component></table-component>
+                        <table-component
+                            :dados="marcas"
+                            :titulos="['ID', 'Nome', 'Imagem']"
+                        ></table-component>
                     </template>
 
                     <template v-slot:rodape>
