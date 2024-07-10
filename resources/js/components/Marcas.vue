c93bdf14c350ede5881af4f7edb6d840b50bcb2f
redersonrodrigues <redersonatalibarodrigues@gmail.com>
2024-07-10 16:42:51 -0300

    373. Corrigindo o atributo marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -34,16 +34,14 @@
                 <!-- início do card de listagem de marcas -->
                 <card-component titulo="Relação de marcas">
                     <template v-slot:conteudo>
-                        <table-component
-                            :dados="marcas.data"
-                            :titulos="{
-                                id:{titulo: 'ID', tipo:'texto'},
-                                nome:{titulo: 'Nome', tipo:'texto'},
-                                imagem:{titulo: 'Imagem', tipo:'imagem'},
-                                created_at:{titulo: 'Criação', tipo:'data'},
-                            }"
-
-                        ></table-component>
+                        <table-component 
+                        :dados="marcas.data" 
+                        :titulos="{
+                            id: { titulo: 'ID', tipo: 'texto' },
+                            nome: { titulo: 'Nome', tipo: 'texto' },
+                            imagem: { titulo: 'Imagem', tipo: 'imagem' },
+                            created_at: { titulo: 'Criação', tipo: 'data' },
+                        }"></table-component>
                     </template>
 
                     <template v-slot:rodape>
@@ -57,8 +55,10 @@
         <modal-component id="modalMarca" titulo="Adicionar marca">
 
             <template v-slot:alertas>
-                <alert-component tipo="success" :detalhes="transacaoDetalhes" titulo="Cadastro realizado com sucesso" v-if="transacaoStatus == 'adicionado'"></alert-component>
-                <alert-component tipo="danger" :detalhes="transacaoDetalhes" titulo="Erro ao tentar cadastrar a marca" v-if="transacaoStatus == 'erro'"></alert-component>
+                <alert-component tipo="success" :detalhes="transacaoDetalhes" titulo="Cadastro realizado com sucesso"
+                    v-if="transacaoStatus == 'adicionado'"></alert-component>
+                <alert-component tipo="danger" :detalhes="transacaoDetalhes" titulo="Erro ao tentar cadastrar a marca"
+                    v-if="transacaoStatus == 'erro'"></alert-component>
             </template>
 
             <template v-slot:conteudo>
@@ -90,88 +90,88 @@
 </template>
 
 <script>
-    export default {
-        computed: {
-            token() {
+export default {
+    computed: {
+        token() {
 
-                let token = document.cookie.split(';').find(indice => {
-                    return indice.includes('token=')
-                })
+            let token = document.cookie.split(';').find(indice => {
+                return indice.includes('token=')
+            })
 
-                token = token.split('=')[1]
-                token = 'Bearer ' + token
+            token = token.split('=')[1]
+            token = 'Bearer ' + token
 
-                return token
-            }
-        },
-        data() {
-            return {
-                urlBase: 'http://localhost:8000/api/v1/marca',
-                nomeMarca: '',
-                arquivoImagem: [],
-                transacaoStatus: '',
-                transacaoDetalhes: {},
-                marcas: []
-            }
-        },
-        methods: {
-            carregarLista() {
-
-                let config = {
-                    headers: {
-                        'Accept': 'application/json',
-                        'Authorization': this.token
-                    }
-                }
-
-                axios.get(this.urlBase, config)
-                    .then(response => {
-                        this.marcas = response.data
-                        console.log(this.marcas)
-                    })
-                    .catch(errors => {
-                        console.log(errors)
-                    })
-            },
-            carregarImagem(e) {
-                this.arquivoImagem = e.target.files
-            },
-            salvar() {
-                console.log(this.nomeMarca, this.arquivoImagem[0])
-
-                let formData = new FormData();
-                formData.append('nome', this.nomeMarca)
-                formData.append('imagem', this.arquivoImagem[0])
-
-                let config = {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                        'Accept': 'application/json',
-                        'Authorization': this.token
-                    }
-                }
-
-                axios.post(this.urlBase, formData, config)
-                    .then(response => {
-                        this.transacaoStatus = 'adicionado'
-                        this.transacaoDetalhes = {
-                            mensagem: 'ID do registro: ' + response.data.id
-                        }
-
-                        console.log(response)
-                    })
-                    .catch(errors => {
-                        this.transacaoStatus = 'erro'
-                        this.transacaoDetalhes = {
-                            mensagem: errors.response.data.message,
-                            dados: errors.response.data.errors
-                        }
-                        //errors.response.data.message
-                    })
-            }
-        },
-        mounted() {
-            this.carregarLista()
+            return token
         }
+    },
+    data() {
+        return {
+            urlBase: 'http://localhost:8000/api/v1/marca',
+            nomeMarca: '',
+            arquivoImagem: [],
+            transacaoStatus: '',
+            transacaoDetalhes: {},
+            marcas: { data: [] }
+        }
+    },
+    methods: {
+        carregarLista() {
+
+            let config = {
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': this.token
+                }
+            }
+
+            axios.get(this.urlBase, config)
+                .then(response => {
+                    this.marcas = response.data
+                    console.log(this.marcas)
+                })
+                .catch(errors => {
+                    console.log(errors)
+                })
+        },
+        carregarImagem(e) {
+            this.arquivoImagem = e.target.files
+        },
+        salvar() {
+            console.log(this.nomeMarca, this.arquivoImagem[0])
+
+            let formData = new FormData();
+            formData.append('nome', this.nomeMarca)
+            formData.append('imagem', this.arquivoImagem[0])
+
+            let config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Accept': 'application/json',
+                    'Authorization': this.token
+                }
+            }
+
+            axios.post(this.urlBase, formData, config)
+                .then(response => {
+                    this.transacaoStatus = 'adicionado'
+                    this.transacaoDetalhes = {
+                        mensagem: 'ID do registro: ' + response.data.id
+                    }
+
+                    console.log(response)
+                })
+                .catch(errors => {
+                    this.transacaoStatus = 'erro'
+                    this.transacaoDetalhes = {
+                        mensagem: errors.response.data.message,
+                        dados: errors.response.data.errors
+                    }
+                    //errors.response.data.message
+                })
+        }
+    },
+    mounted() {
+        this.carregarLista()
     }
+}
 </script>
